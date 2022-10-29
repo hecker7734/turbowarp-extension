d28b330072f5bf7f70e0612c2d0b12e694d443d4
hecker7734 <heckerishere7735@gmail.com>
2022-10-29 18:15:01 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -59,7 +59,18 @@ class morestuffExtension {
     return args.ONE == args.TWO;
   }
   RootOf(args) {
-    return Math.pow(args.ONE, 1,args.TWO);
+    function nthroot(x, n) {
+      try {
+        var negate = n % 2 == 1 && x < 0;
+        if(negate)
+          x = -x;
+        var possible = Math.pow(x, 1 / n);
+        n = Math.pow(possible, n);
+        if(Math.abs(x - n) < 1 && (x > 0 == n > 0))
+          return negate ? -possible : possible;
+      } catch(e){}
+    }
+    return nthroot(args.ONE,args.TWO);
   }
 }
 Scratch.extensions.register(new morestuffExtension());
