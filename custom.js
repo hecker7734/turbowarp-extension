24984d401e837f3b8a88857ef2ced74a54e2b42a
hecker7734 <heckerishere7735@gmail.com>
2022-10-29 17:56:06 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,8 +1,8 @@
 class morestuffExtension {
   getInfo() {
     return {
-      id: 'morestuff', // change this if you make an actual extension!
-      name: 'Strict Equality',
+      id: 'morestuff',
+      name: 'More Stuff',
       blocks: [
         {
           opcode: 'strictlyEquals',
@@ -18,13 +18,30 @@ class morestuffExtension {
               defaultValue: 'n'
             }
           }
+        },
+        {
+          opcode: 'LooslyEquals',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] = [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'n'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'n'
+            }
+          }
         }
       ]
     };
   }
   strictlyEquals(args) {
-    // Note strict equality: Inputs must match exactly: in type, case, etc.
     return args.ONE === args.TWO;
+  }
+  LooslyEquals(args) {
+    return args.ONE = args.TWO;
   }
 }
 Scratch.extensions.register(new morestuffExtension());
